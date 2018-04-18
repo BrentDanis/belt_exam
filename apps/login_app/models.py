e4bee8a6fc9250ea92bc59ab5c22d59b8e800939
Brent <brentdanis@gmail.com>
2018-04-18 11:48:06 -0700

    11:45 updates

--- apps/login_app/models.py
+++ apps/login_app/models.py
@@ -1,5 +1,6 @@
 from __future__ import unicode_literals
 from django.db import models
+from django import forms
 
 #adding data base structure (think excel headers)
 
@@ -8,3 +9,5 @@ class Users(models.Model):
 	last_name=models.CharField(max_length=50)
 	email=models.EmailField()
 	password=models.CharField(max_length=255)
+	def __str__(self):
+		return "<Users object: {} {}>".format(self.first_name, self.last_name)
